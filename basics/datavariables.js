3bd1313e9a25f490d351349b80b99eaa8cd625f3
Harsh <159182118+Hars311@users.noreply.github.com>
2024-06-01 18:28:51 +0000

    data types

---
+++ basics/datavariables.js
@@ -0,0 +1,25 @@
+"use strict" /*
+treat all code as new */
+//alert(3+3) // we are using in node
+// code readability should be high 
+
+let name = "harsh"
+let age = 20
+let isloggedIn = true
+let state = null
+
+//Data types (primitive data types)
+//number 
+//bigint -big number
+//string - can use in single or doublr quotes
+//boolean - true or false
+//null- stand alone value
+//undefined 
+
+//symbol - used in react and figma tools unique 
+
+//object 
+console.log(typeof "harsh")
+
+console. log(typeof undefined); // undefined
+console. log(typeof null); // object
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,8 @@
-console.log("harsh")
+const accountId = 434534
+var accountEmail = "jarsj@gmail.com"
+let accountName = "Harsh Thakur"
+accountcity = "jaipur"
+accountEmail = "Harshthakur@gmail.com"
+console.log(accountEmail);
+
+console.table([accountId , accountEmail, accountcity ])
