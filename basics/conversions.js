b99d870ddb3a54600c9920f77c5c4bd7ad24a110
Harsh <159182118+Hars311@users.noreply.github.com>
2024-06-02 18:29:46 +0000

    conversions

---
+++ basics/conversions.js
@@ -0,0 +1,16 @@
+let score = "33av"
+console.log(typeof score)
+let valueInNumber = Number(score)
+ console.log(valueInNumber)
+ console.log(typeof valueInNumber)
+
+ //number is easily convertiable 
+ // number with string gives Nan and type is number
+ // true is 1 false is 0
+
+ let isLoggedIN = 1
+
+ let booleaninnumber =Boolean(isLoggedIN)
+
+ console.log(typeof)
+ 
